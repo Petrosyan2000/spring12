a04cfe9d1eecd055282bddfc5f805da3edac17c8
Garik Petrosyan <pgarik62@gmail.com>
2022-04-24 19:08:32 +0400

    t

---
+++ src/main/java/com/example/myfirstspringproject/controler/Index.java
@@ -0,0 +1,13 @@
+package com.example.myfirstspringproject.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Index {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
